89c1a811343ec416dcd556395824483c29e3d225
dartsmith <melissa.smith444@gmail.com>
2015-12-27 15:53:16 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## Programmer: dartsmith
+## December 27, 2015
+## Written for Assignment 2, Coursera R Programming course
+##
+## This program includes a pair of functions that cache the inverse of a matrix.
+##     Functions: makeCacheMatrix()
+##                cacheSolve()
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {  
+    invmat <- NULL
+    setm <- function(y) {
+        x <<- y
+        invmat <<- NULL                   # Initialize inverse matrix
+    }
+    
+    getm <- function() x
+    setinv <- function(imatt) invmat <<- imatt
+    getinv <- function() invmat
+    list(setm = setm, getm = getm,
+         setinv = setinv,
+         getinv = getinv)   
+}    
 
-}
 
+## This function computes the inverse of the special "matrix" returned 
+##    by makeCacheMatrix above. If the inverse has already been calculated 
+##    (and the matrix has not changed), then the cachesolve should 
+##    retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    invmat <- x$getinv()
+    if(!is.null(invmat)) {                 # Do when inverse has been found before
+        message("getting cached inverse")
+        return(invmat)
+    }
+    
+    tempm <- x$getm()                 # Do when inverse has not been found before
+    invmat <- solve(tempm)          
+    x$setinv(invmat)
+    message("not cached")
+    invmat
+    
 }
